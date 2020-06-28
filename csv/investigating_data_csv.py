e576b2df4a3bc294020d3b1dd25479aa34886267
skorudzhiev <skorudzhiev@gmail.com>
2020-06-28 10:55:25 +0300

    Create a new dictionary with a specified data
    - Creating a new dictionary from a pre-existing one to select only the
    necessary data

--- csv/investigating_data_csv.py
+++ csv/investigating_data_csv.py
@@ -90,3 +90,22 @@ non_udacity_submissions = remove_udacity_accounts(project_submissions)
 print(len(non_udacity_enrollments))
 print(len(non_udacity_engagement))
 print(len(non_udacity_submissions))
+
+
+## Create a dictionary named paid_students containing all students who either
+## haven't canceled yet or who remained enrolled for more than 7 days. The keys
+## should be account keys, and the values should be the date the student enrolled.
+
+paid_students = {}
+
+for enrollment in non_udacity_enrollments:
+    if not enrollment['is_canceled'] or enrollment['days_to_cancel'] > 7:
+        account_key = enrollment['account_key']
+        enrollment_date = enrollment['join_date']
+
+        if account_key not in paid_students or \
+            enrollment_date > paid_students[account_key]:
+            paid_students[account_key] = enrollment_date
+
+
+len(paid_students)

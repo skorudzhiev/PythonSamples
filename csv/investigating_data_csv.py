2f64587c9ba93ca4837d78d6d03889a3af4b8dd1
skorudzhiev <skorudzhiev@gmail.com>
2020-06-28 10:17:09 +0300

    Looping with break statement
    Solving the first problem to find any student enrollments where
    the student is missing from the daily engagement table
    
    - Looping through the tables with variety of conditions to identify
    potential problems
    - Deleting specifing records which appeared to be unnecessary for the
    data analysis.

--- csv/investigating_data_csv.py
+++ csv/investigating_data_csv.py
@@ -41,5 +41,52 @@ unique_project_submissions = set()
 for submission in project_submissions:
     unique_project_submissions.add(submission['account_key'])
 len(unique_project_submissions)
-
 print(len(unique_engagement_students))
+
+## Find any one student enrollments where the student is missing from the daily engagement table.
+## Output that enrollment.
+
+for enrollment in enrollments:
+    student = enrollment['account_key']
+    if student not in unique_engagement_students:
+        print(enrollment) 
+        break
+
+
+## Find the number of surprising data points (enrollments missing from
+## the engagement table) that remain, if any.
+
+num_problem_students = 0
+
+for enrollment in enrollments:
+    student = enrollment['account_key']
+    if student not in unique_engagement_students \
+        and enrollment['join_date'] != enrollment['cancel_date']:
+        num_problem_students += 1
+
+num_problem_students
+
+
+# Create a set of the account keys for all Udacity test accounts
+udacity_test_accounts = set()
+for enrollment in enrollments:
+    if enrollment['is_udacity']:
+        udacity_test_accounts.add(enrollment['account_key'])
+len(udacity_test_accounts)
+
+# Given some data with an account_key field, removes any records corresponding to Udacity test accounts
+def remove_udacity_accounts(data):
+    non_udacity_data = []
+    for data_point in data:
+        if data_point['account_key'] not in udacity_test_accounts:
+            non_udacity_data.append(data_point)
+    return non_udacity_data
+
+# Remove Udacity test accounts from all three tables
+non_udacity_enrollments = remove_udacity_accounts(enrollments)
+non_udacity_engagement = remove_udacity_accounts(daily_engagement)
+non_udacity_submissions = remove_udacity_accounts(project_submissions)
+
+print(len(non_udacity_enrollments))
+print(len(non_udacity_engagement))
+print(len(non_udacity_submissions))

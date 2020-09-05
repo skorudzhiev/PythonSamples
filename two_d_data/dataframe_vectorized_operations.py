8a31e4964b9cec84e50bc36f64bd5001bf19f244
skorudzhiev <skorudzhiev@gmail.com>
2020-09-05 10:53:47 +0300

    DataFrame Vectorized Operations

---
+++ two_d_data/dataframe_vectorized_operations.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Examples of vectorized operations on DataFrames:
+# Change False to True for each block of code to see what it does
+
+# Adding DataFrames with the column names
+if False:
+    df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]})
+    df2 = pd.DataFrame({'a': [10, 20, 30], 'b': [40, 50, 60], 'c': [70, 80, 90]})
+    print(df1 + df2)
+    
+# Adding DataFrames with overlapping column names 
+if False:
+    df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]})
+    df2 = pd.DataFrame({'d': [10, 20, 30], 'c': [40, 50, 60], 'b': [70, 80, 90]})
+    print(df1 + df2)
+
+# Adding DataFrames with overlapping row indexes
+if False:
+    df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]},
+                       index=['row1', 'row2', 'row3'])
+    df2 = pd.DataFrame({'a': [10, 20, 30], 'b': [40, 50, 60], 'c': [70, 80, 90]},
+                       index=['row4', 'row3', 'row2'])
+    print(df1 + df2)

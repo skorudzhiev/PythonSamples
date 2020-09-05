b22737ce813e06dfc0fc687b9c2d98e95e7071ee
skorudzhiev <skorudzhiev@gmail.com>
2020-09-05 13:19:31 +0300

    DataFrame apply()

---
+++ two_d_data/dataframe_apply.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+grades_df = pd.DataFrame(
+    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
+          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
+    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio', 
+           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
+)
+
+# DataFrame apply()
+def convert_grades_curve(exam_grades):
+    # Pandas has a bult-in function that will perform this calculation
+    # This will give the bottom 0% to 10% of students the grade 'F',
+    # 10% to 20% the grade 'D', and so on. You can read more about
+    # the qcut() function here:
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.qcut.html
+    return pd.qcut(exam_grades,
+                   [0, 0.1, 0.2, 0.5, 0.8, 1],
+                   labels=['F', 'D', 'C', 'B', 'A'])
+    
+# qcut() operates on a list, array, or Series. This is the
+# result of running the function on a single column of the
+# DataFrame.
+print(convert_grades_curve(grades_df['exam1']))
+
+# qcut() does not work on DataFrames, but we can use apply()
+# to call the function on each column separately
+print(grades_df.apply(convert_grades_curve))
+
+
+df = pd.DataFrame({
+    'a': [4, 5, 3, 1, 2],
+    'b': [20, 10, 40, 50, 30],
+    'c': [25, 20, 5, 15, 10]
+})
+
+# Change False to True for this block of code to see what it does
+# DataFrame apply() - use case 2
+if False:   
+    print(df.apply(np.mean))
+    print(df.apply(np.max))

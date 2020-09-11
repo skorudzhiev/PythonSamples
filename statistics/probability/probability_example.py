fb6e067b32da0459ca0e9f96c5f9bccfc5c46cce
skorudzhiev <skorudzhiev@gmail.com>
2020-09-11 15:13:15 +0300

    Example of the Bayes Rule on negative outcome

--- statistics/probability/probability_example.py
+++ statistics/probability/probability_example.py
@@ -29,7 +29,15 @@ Example of the Bayes Rule on positive result
 '''
 #Return the probability of A conditioned on B given that 
 #P(A)=p0, P(B|A)=p1, and P(Not B|Not A)=p2 
-def bayes_rule(p0, p1, p2):
+def bayes_rule_positive(p0, p1, p2):
     return p0 * p1 / (p0 * p1 + (1 - p0) * (1 - p2))
 
-print(bayes_rule(0.1, 0.9, 0.8))
+'''
+Example of the Bayes Rule on negative result
+'''
+#Return the probability of A conditioned on Not B given that 
+#P(A)=p0, P(B|A)=p1, and P(Not B|Not A)=p2 
+def bayes_rule_negative(p0, p1, p2):
+    return p0 * (1 - p1) / (p0 * (1 - p1) + (1 - p0) * p2)
+
+print(bayes_rule_negative(0.1, 0.9, 0.8))

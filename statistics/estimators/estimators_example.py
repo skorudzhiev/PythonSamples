939e3aa2f754e1ad305effb7583fc681af343539
skorudzhiev <skorudzhiev@gmail.com>
2020-09-12 14:42:28 +0300

    Example of function returning a Median

--- statistics/estimators/estimators_example.py
+++ statistics/estimators/estimators_example.py
@@ -1,4 +1,5 @@
 data1=[49., 66, 24, 98, 37, 64, 98, 27, 56, 93, 68, 78, 22, 25, 11]
+data2=[1,2,5,10,-20]
 
 # Return the mean value of a list of numbers 
 # which can be also used to represent
@@ -6,4 +7,11 @@ data1=[49., 66, 24, 98, 37, 64, 98, 27, 56, 93, 68, 78, 22, 25, 11]
 def mean(data):
     return sum(data) / len(data)
 
-print(mean(data1))
+
+# Return the median of a list of numbers 
+def median(data):
+    sdata = sorted(data)
+    index = (len(data) - 1) // 2
+    return sdata[index]
+
+print(median(data2))
